8c66e73710dc0a7c4a233819d526d5e9a09854e4
Shrinath Kumar <shrinath.kumar@ucdconnect.ie>
2021-11-23 16:24:31 +0000

    Added code in Sum.c as indicated in the exercise to compute the sum of the first 1000 terms in the harmonic series forwards and backwards

--- practical02/Sum.c
+++ practical02/Sum.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-
 int main(void) {
 /* Declare variables */
    int i;
    float sum1, sum2, diff;
-   
 
 /* First sum */
    sum1 = 0.0;
    for (i=1; i<=1000; i++) {
-      /*  Insert here */
+   //printf("sum1: %f, adding: %f", sum1, 1/i)
+   // need 1.0 here instead of 1 -> float vs int division
+	sum1 += 1.0/i;
    }
 
 
@@ -18,13 +18,14 @@ int main(void) {
    sum2 = 0.0;
    for (i=1000; i>0; i--) {
       /* Insert the same line as above except use sum2 */
+     sum2 += 1.0/i;
    }
 
    printf(" Sum1=%f\n",sum1);
    printf(" Sum2=%f\n",sum2);
 
 /* Find the difference */
-   diff = /* ?? */
+   diff = sum2 - sum1;
 
    printf(" Difference between the two is %f\n",diff);
 
